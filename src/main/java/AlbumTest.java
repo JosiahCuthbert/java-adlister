41ba42f8f2c0be853e974edd001048531adc7eae
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-01-24 11:45:29 -0600

    dao ex complete

---
+++ src/main/java/AlbumTest.java
@@ -0,0 +1,13 @@
+public class AlbumTest {
+
+    public static void main(String[] args) {
+        Album whiteAlbum = new Album();
+
+        whiteAlbum.setArtist("The Beatles");
+        whiteAlbum.setGenre("Rock");
+        whiteAlbum.setName("The Beatles (White Album)");
+        whiteAlbum.setRelease_date(1968);
+        whiteAlbum.setSales(14.3);
+    }
+    
+}
